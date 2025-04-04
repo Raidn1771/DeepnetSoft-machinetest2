da266174bec96424f837ccc42d27b96fa2c018a9
RishenJessilFigarez <61039771+Raidn1771@users.noreply.github.com>
2025-04-04 13:00:34 +0530

    Update index.jsx

--- frontend/Pages/Home/index.jsx
+++ frontend/Pages/Home/index.jsx
@@ -192,7 +192,7 @@ const Home = () => {
               ) : (
                 <p className="noItems">
                   No items found.
-                  <button onClick={() => setIsModalOpen(true)}>
+                  <button className="insideMenuButton" onClick={() => setIsModalOpen(true)}>
                     Add the first item
                   </button>
                 </p>
